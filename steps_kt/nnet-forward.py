7eefbe775a01f8332d899d3faffca71781f6af09
D S Pavan Kumar <dspavankumar@users.noreply.github.com>
2016-08-01 17:30:45 +0530

    Create nnet-forward.py

---
+++ steps_kt/nnet-forward.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+##  Copyright (C) 2016 D S Pavan Kumar
+##  dspavankumar [at] gmail [dot] com
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import sys
+import numpy
+import keras
+import kaldiIO
+from signal import signal, SIGPIPE, SIG_DFL
+
+if __name__ == '__main__':
+    model = sys.argv[1]
+
+    if not model.endswith('.h5'):
+        raise TypeError ('Unsupported model type. Please use h5 format. Update Keras if needed')
+
+    ## Load model
+    m = keras.models.load_model (model)
+
+    arkIn = sys.stdin.buffer
+    arkOut = sys.stdout.buffer
+    encoding = sys.stdout.encoding
+    signal (SIGPIPE, SIG_DFL)
+
+    uttId, featMat = kaldiIO.readUtterance(arkIn)
+    logProbMat = numpy.log (m.predict (featMat))
+    while uttId:
+        kaldiIO.writeUtterance(uttId, logProbMat, arkOut, encoding)
+        uttId, featMat = kaldiIO.readUtterance(arkIn)
+    
